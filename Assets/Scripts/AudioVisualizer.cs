4ce034aca9bedc0491e485c54e6c7f469cd7e112
KVY <kvy@phygitalism.com>
2020-05-03 13:07:02 +0300

    init project

---
+++ Assets/Scripts/AudioVisualizer.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+  
+public class AudioVisualizer : MonoBehaviour  
+{   
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private LineRenderer lineRenderer; 
+    [SerializeField] private GameObject trailPrefab;
+    [SerializeField] [Range(6, 13)] private int samplesSizePower = 4;
+    [SerializeField] [Range(0f, 100f)] private float power = 1f;
+    [SerializeField] private float scale = 100f;
+    [SerializeField] private int length = 100;
+    [SerializeField] [Range(1, 100)]  private int trailsDivider = 50;
+    
+    private GameObject[] audioTrailObjects;
+    private Vector3 _samplePointPosition;
+    private float[] _samples;
+    private float _samplesSize;
+  
+    private void Start()
+    {
+        _samplesSize = Mathf.Pow(2, samplesSizePower);
+        _samples = new float[(int)_samplesSize];
+
+        lineRenderer.positionCount = Mathf.Clamp(_samples.Length, 0, length); 
+
+        audioSource.Play();
+
+        audioTrailObjects = new GameObject[length / trailsDivider];
+        SpawnTrails();
+    }
+  
+    private void Update()
+    { 
+        //lineRenderer.positionCount = Mathf.Clamp(_samples.Length, 0, length); 
+        audioSource.GetSpectrumData(_samples, 0, FFTWindow.BlackmanHarris);  
+
+        for(int i = 0; i < length; i++)  
+        {  
+            _samplePointPosition.Set
+            (
+                0, 
+                Mathf.Clamp(_samples[i]  * power, 0, 50), 
+                scale / (_samples.Length - 1) * i
+            ); 
+  
+            lineRenderer.SetPosition(i, _samplePointPosition);  
+        }
+
+        SetTrailsPosition();
+    }
+
+    private void SpawnTrails()
+    {
+        for (int i = 0; i < audioTrailObjects.Length; i++)
+        {
+            audioTrailObjects[i] = Instantiate(
+                trailPrefab, 
+                new Vector3(0, 0, scale / (_samples.Length - 1) * i * trailsDivider),
+                Quaternion.identity
+                );
+        }
+    }
+
+    private void SetTrailsPosition()
+    {
+        for (int i = 0; i < audioTrailObjects.Length; i++)
+        {
+            float height = Mathf.Clamp(_samples[i * trailsDivider]  * power, 0, 50); 
+            audioTrailObjects[i].transform.position = new Vector3(
+                -audioSource.time, height, audioTrailObjects[i].transform.position.z);
+        }
+    }
+}
---
+++ Assets/Shaders/HeightColor.shader
@@ -0,0 +1,60 @@
+﻿Shader "Unlit/HeightColor"
+{
+    Properties
+    {
+        _ColorBottom ("Bottom Color", Color) = (0, 0, 0, 0)
+        _ColorTop ("Top Color", Color) = (1, 1, 1, 1) 
+    }
+    SubShader
+    {
+        Tags 
+        { 
+            "RenderType"="Transparent" 
+            "Queue"="Transparent" 
+        }
+
+        Blend SrcAlpha OneMinusSrcAlpha
+        ZWrite off
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct v2f
+            {
+                float2 worldPos : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 svPos : SV_POSITION;
+            };
+
+            fixed4 _ColorBottom, _ColorTop;
+
+            v2f vert (float4 localPos : POSITION)
+            {
+                v2f o;
+
+                o.svPos = UnityObjectToClipPos(localPos);
+                o.worldPos = mul(unity_ObjectToWorld, localPos);
+
+                UNITY_TRANSFER_FOG(o,o.svPos);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = lerp(_ColorBottom, _ColorTop, i.worldPos.y / 10);
+                // apply fog
+                UNITY_APPLY_FOG(i.fogCoord, col);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
